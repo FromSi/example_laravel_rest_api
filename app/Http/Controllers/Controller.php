e64d268d73bd04df1717427bf83b61b6da86807b
FromSi <fromsi665@gmail.com>
2022-07-21 23:19:54 +0600

    feat(config): add paginate for api

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,14 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Получить число для пагинации
+     *
+     * @return int
+     */
+    protected function getApiPaginate(): int
+    {
+        return config('api.paginate');
+    }
 }
---
+++ config/api.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'paginate' => env('API_PAGINATE', 15)
+];
